2dd757af550ece8bbb75c2912abffd6aa5487ffe
RodriJOk <juarezrodrigo59@gmail.com>
2024-11-08 23:35:50 -0300

    Modificaciones en los esquemas de migraciones

--- database/migrations/2024_10_30_210457_suscription.php
+++ database/migrations/2024_10_30_210457_suscription.php
@@ -11,15 +11,19 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('suscription', function(Blueprint $table){
+        Schema::create('suscription', function (Blueprint $table) {
             $table->increments('id')->nullable(false);
             $table->string('name', 255)->nullable(false);
-            $table->enum('type', ['mensual', 'anual'])->nullable(false);
-            $table->timestamp('start_date')->nullable();
+            $table->string('type', 10)->nullable(false); // Cambia enum a string con longitud adecuada
+            $table->timestamp('start_date');
             $table->timestamp('end_date')->nullable();
-            $table->enum('activo', ['si', 'no'])->nullable(false);
+            $table->string('activo', 2)->nullable(false); // Cambia enum a string con longitud adecuada
             $table->integer('id_pago')->nullable(false);
             $table->integer('id_cliente')->nullable(false);
+
+            // Restricciones de check para simular el enum
+            $table->check("type in ('mensual', 'anual')");
+            $table->check("activo in ('si', 'no')");
         });
     }
 
