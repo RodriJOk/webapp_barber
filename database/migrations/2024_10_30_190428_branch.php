99fa232dbe2b844f90055d6025d5f3c116fc12a5
RodriJOk <juarezrodrigo59@gmail.com>
2024-11-08 22:56:06 -0300

    Agrego nullable en true

--- database/migrations/2024_10_30_190428_branch.php
+++ database/migrations/2024_10_30_190428_branch.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->bigIncrements('id'); // O usa increments si prefieres enteros más pequeños
             $table->string('name', 255)->nullable(false);
             $table->string('email', 255)->unique()->nullable(false);
-            $table->timestamp('email_verified_at')->nullable(); // Campo separado, sin duplicación
+            $table->timestamp('email_verified_at')->nullable(false);
             $table->string('password', 255)->nullable(false);
             $table->rememberToken(); // Corrige a rememberToken()
             $table->timestamps(); // Este método agrega `created_at` y `updated_at` automáticamente
