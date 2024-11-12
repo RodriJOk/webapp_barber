8aa1f5834ddea767c9541ca006f033fbad77485b
RodriJOk <juarezrodrigo59@gmail.com>
2024-11-11 22:24:52 -0300

    Agrego la migracion de la tabla roles

---
+++ database/migrations/2024-11-11_213456_roles.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('name')->nullable(false);
+            $table->dateTime('created_at')->nullable();
+            $table->dateTime('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+    }
+};
