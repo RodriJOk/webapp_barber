655a0dbaf0f8edf5896a628bc9eefe89f8bb13e7
Alan Rodrigo Juarez <rodrigo.juarez@credencial.com.ar>
2024-12-04 19:01:25 -0300

    Agrego la seccion de Google Analitycs en la seccion de index y la home.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -333,4 +333,14 @@
             }
         }
     </script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-0JRDR6DM4F"></script>
+    <script>
+        //Agrego el acceso a Google Analitycs
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-0JRDR6DM4F');
+    </script>
 </html>
