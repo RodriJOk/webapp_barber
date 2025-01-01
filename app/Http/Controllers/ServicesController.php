97c4c0d34ca116d3090d444b28b0b6b9c60b8ba4
RodriJOk <juarezrodrigo59@gmail.com>
2025-01-01 13:58:39 -0300

    Agrego el model y los controlers de la seccion services

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -10,8 +10,37 @@ class ServicesController extends Controller
 {
     public function my_services()
     {
-        $branch_id = session()->get('branch_id');
-        $myServicesByIdBranch = Services::getServicesByBranch($branch_id);
-        return view('services.index', ['myServicesByIdBranch' => $myServicesByIdBranch]);
+        $id_user = session()->get('id_usuario');
+        $myServicesByBranch = Services::getServicesByIdUser($id_user);
+        return view('services.index', ['myServicesByBranch' => $myServicesByBranch]);
+    }
+    public function save_service()
+    {
+        $validator = Validator::make(request()->all(), [
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required',
+            'duration' => 'required|numeric',
+            'state' => 'required|in:activo,inactivo'
+        ]);
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+        
+        $data = [
+            'name' => request()->name,
+            'description' => request()->description,
+            'price' => request()->price,
+            'duration' => request()->duration,
+            'state' => request()->state
+        ];
+
+        if(!Services::saveService($data)){
+            toast()->error('Error al guardar el servicio');
+            return redirect()->route('my_services');
+        }
+
+        toast()->success('Servicio guardado correctamente');
+        return redirect()->route('my_services');
     }
 }
--- app/Models/Services.php
+++ app/Models/Services.php
@@ -20,12 +20,30 @@ class Services extends Authenticatable{
         'updated_at',
         'price',
         'duration',
+        'estado'
     ];
 
-    protected function getServicesByBranch($branch_id){
-        $services = DB::table('services')
-            ->where('branch_id', $branch_id)
-            ->get();
+    protected function getServicesByIdUser($id_user)
+    {
+        $services = DB::table('branch')
+                    ->join('services', 'branch.id', '=', 'services.branch_id')
+                    ->where('branch.id_user', $id_user)
+                    ->select('services.*', 'branch.id as branch_id', 'branch.name as branch_name')
+                    ->get();
         return $services;
+    }
+
+    protected function saveService($data){
+        $service = new Services();
+        $service->name = $data['name'];
+        $service->description = $data['description'];
+        $service->branch_id = $data['branch_id'];
+        $service->created_at = date('Y-m-d H:i:s');
+        $service->updated_at = date('Y-m-d H:i:s');
+        $service->price = $data['price'];
+        $service->duration = $data['duration'];
+        $service->state = $data['state'];
+        $service->save();
+        return true;
     }
 }
